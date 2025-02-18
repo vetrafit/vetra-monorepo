7bd75ea4a3017f61879ca33aadbb2314b0fcb2a8
Zaq Reynolds <zaqreynolds@gmail.com>
2025-02-17 19:39:36 -0800

    feat(ui): update subproject and add new assets
    - Updated subproject commit reference.
    - Added two new SVG images for fitness stats and personal training.
    - Simplified exports in the index file to streamline component imports.

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,12 +1,5 @@
 import "./styles/globals.css";
 
-export * from "./components/button";
-export * from "./components/alert-dialog";
-export * from "./components/calendar";
-// ... export other components
-
-// Or if you prefer a single line for all components:
-// export * from "./components/*";
-
+export * from "./components/index";
 export * from "./lib/utils";
 export * from "./hooks";
